eb12dc0b82b9fd89bc3238b84d83b1d09a3f1b07
cha-yh <chayh311@gmail.com>
2020-03-23 11:36:01 +0900

    Add exporting Loader component

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,5 @@ export { default as InputRange } from './InputRange/InputRange';
 export { default as Notification } from './Notification/Notification';
 export { default as Popup } from './Popup/Popup';
 export { default as Textarea } from './Textarea/Textarea';
+export { default as Loader } from './Loader/Loader';
 export {media, palette, rem, utils, flex} from './utils';
