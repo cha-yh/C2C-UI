d251e0c95dd5db0d68de2230d04c80367abe9b6b
cha-yh <chayh311@gmail.com>
2020-03-23 18:59:48 +0900

    Update ButtonGroup docs

--- src/ButtonGroup/ButtonGroup.stories.tsx
+++ src/ButtonGroup/ButtonGroup.stories.tsx
@@ -32,36 +32,60 @@ buttonGroup.story = {
 
 export const rightAlign = () => {
   return (
-    <ButtonGroup rightAlign>
-      <Button theme="tertiary">취소</Button>
-      <Button>확인</Button>
-    </ButtonGroup>
+    <>
+      <p>rightAlign={'true'}</p>
+      <ButtonGroup rightAlign>
+        <Button theme="tertiary">Cancel</Button>
+        <Button>Confirm</Button>
+      </ButtonGroup>
+    </>
   );
 };
 
 export const column = () => {
   return (
-    <ButtonGroup direction="column">
-      <Button>CLICK ME</Button>
-      <Button>CLICK ME</Button>
-    </ButtonGroup>
+    <>
+      <p>direction="column"</p>
+      <ButtonGroup direction="column">
+        <Button>CLICK ME</Button>
+        <Button>CLICK ME</Button>
+      </ButtonGroup>
+    </>
   );
 };
 
 export const customGap = () => {
   return (
-    <ButtonGroup gap={16}>
-      <Button theme="tertiary">취소</Button>
-      <Button>확인</Button>
-    </ButtonGroup>
+    <>
+      <p>{`gap={16}`}</p>
+      <ButtonGroup gap={16}>
+        <Button theme="tertiary">Cancel</Button>
+        <Button>Confirm</Button>
+      </ButtonGroup>
+    </>
   );
 };
 
 export const customGapColumn = () => {
   return (
-    <ButtonGroup direction="column" gap={16}>
-      <Button>CLICK ME</Button>
-      <Button>CLICK ME</Button>
-    </ButtonGroup>
+    <>
+      <p>{`direction="column" gap={16}`}</p>
+      <ButtonGroup direction="column" gap={16}>
+        <Button>CLICK ME</Button>
+        <Button>CLICK ME</Button>
+      </ButtonGroup>
+    </>
   );
 };
+
+export const columnWithWidth = () => {
+  return (
+    <>
+      <p>direction="column" width="20rem"</p>
+      <ButtonGroup direction="column" width="20rem">
+        <Button>CLICK ME</Button>
+        <Button>CLICK ME</Button>
+      </ButtonGroup>
+    </>
+  )
+}
