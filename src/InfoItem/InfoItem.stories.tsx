4d1f280b2bdd49ade5d9c2855e468ed85b0f9c3f
cha-yh <chayh311@gmail.com>
2020-03-25 14:07:46 +0900

    Update InfoItem story

--- src/InfoItem/InfoItem.stories.tsx
+++ src/InfoItem/InfoItem.stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import InfoItem from './InfoItem';
 import { withKnobs, text, boolean, radios, number } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import { rem, palette } from '../utils';
 
 export default {
     title: 'components|InfoItem',
@@ -26,32 +28,84 @@ export const infoItem = () => {
                 row={row}
                 labelWidth={labelWidth}
             />
-
-            <InfoItem
-                label={"label"}
-                value={"value"}
-                row={true}
-                labelWidth={labelWidth}
-            />
-
-            <InfoItem
-                label={"a"}
-                value={"b"}
-                row
-                labelWidth={labelWidth}
-            />
-
-            <div style={{width: '300px', background: 'green'}}>
-                <InfoItem
-                    label={"width test"}
-                    value={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-                    row
-                    labelWidth={labelWidth}
-                />
-            </div>
         </React.Fragment>
     );
 };
+
+export const direction = () => {
+    return (
+        <Box>
+            <h5>{`row={true}`}</h5>
+            <InfoItem
+                label={'label'}
+                value={'value'}
+                row={true}
+            />
+
+            <h5>{`row={false}`}</h5>
+            <InfoItem
+                label={'label'}
+                value={'value'}
+                row={false}
+            />
+        </Box>
+    )
+}
+
+export const labelWidth = () => {
+    return (
+        <Box>
+            <h5>{`row={true} labelWidth={200}`}</h5>
+            <InfoItem
+                label={'lorem'}
+                value={'value1'}
+                row={true}
+                labelWidth={200}
+            />
+            <InfoItem
+                label={'lorem ipsum'}
+                value={'value2'}
+                row={true}
+                labelWidth={200}
+            />
+            <InfoItem
+                label={'lo'}
+                value={'value3'}
+                row={true}
+                labelWidth={200}
+            />
+
+            <h5>{`row={false} labelWidth={100}`}</h5>
+            <InfoItem
+                label={'lor'}
+                value={'value1'}
+                row={true}
+                labelWidth={100}
+            />
+            <InfoItem
+                label={'lorem ipsum'}
+                value={'value2'}
+                row={true}
+                labelWidth={100}
+            />
+            <InfoItem
+                label={'lorem'}
+                value={'value3'}
+                row={true}
+                labelWidth={100}
+            />
+        </Box>
+    )
+}
+
+const Box = styled.div`
+    >h5 {
+        margin: 0;
+        margin-top: ${rem(20)}; 
+        color: ${palette.blue700};
+        font-size: ${rem(24)};
+    }
+`;
 
 infoItem.story = {
     name: 'Default'
